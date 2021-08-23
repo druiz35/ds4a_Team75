0290522f0c3d06e04cecace227e912b33f597474
seguzmanro <sebas32294@gmail.com>
2021-08-23 12:01:50 -0500

    Updated the lineplot to show good municipality names; updated the strip plot with the updated modeling dataframe

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ from plotly.subplots import make_subplots
 
 app = dash.Dash(__name__)
 
-data = pd.read_csv('df_vars_modelamiento.csv')
-investments = pd.melt(data, id_vars=['anio_corte','municipio'], value_vars=['inversion_transformacion','inversion_conectividad', 'inversion'])
-municipalities = list(investments['municipio'].unique())
+data = pd.read_csv('../data/df_vars_modelamiento.csv')
+investments = pd.melt(data, id_vars=['anio_corte','municipio_fixed_name'], value_vars=['inversion_transformacion','inversion_conectividad', 'inversion'])
+municipalities = list(investments['municipio_fixed_name'].unique())
 
 init_map = open('../data/maps_html/map_foliumTotal2016.html','r').read()
 
 # ***** START OF STRIPPLOT *****
 x_options_stripplot = [
-    {"label": u"Año Corte", "value": "anio_corte"},
-    {"label": "Municipio", "value": "municipio"},
-    {"label": "Departamento", "value": "departamento"},
+    {"label": "Año Corte", "value": "anio_corte"},
+    {"label": "Municipio", "value": "municipio_fixed_name"},
+    {"label": "Departamento", "value": "departamento_fixed_name"},
     {"label": "Grupo Dotaciones", "value": "grupo_dotaciones"},
     {"label": "Categoria de Ruralidad", "value": "categoria_de_ruralidad"}
 ]
@@ -30,7 +30,7 @@ y_options_stripplot = []
 for column_name in list(data.columns)[6:]:
     cleaned_column_name = column_name.replace("_", " ")
     label_dict = {"label": cleaned_column_name.title(),"value": column_name}
-    y_options.append(label_dict)
+    y_options_stripplot.append(label_dict)
 
 stripplot = [
     html.Label("X Axis"),
@@ -42,7 +42,7 @@ stripplot = [
     dcc.Dropdown(
         id = "y_options_menu_stripplot",
         options = y_options_stripplot,
-        value="cobertura_media_neta"
+        value="cobertura_neta_en_educacion_media"
     ),
     dcc.Graph(id="stripplot")]
 # ***** END OF STRIPPLOT *****
@@ -91,20 +91,20 @@ def update_line_chart(value):
     #     x="anio_corte", y="value", color='variable')
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(
-        go.Scatter(x=data[data['municipio']==value]["anio_corte"], y=data[data['municipio']==value]["inversion_transformacion"], name="Inversión MinTC - Transformación"),
+        go.Scatter(x=data[data['municipio_fixed_name']==value]["anio_corte"], y=data[data['municipio_fixed_name']==value]["inversion_transformacion"], name="Inversión MinTC - Transformación"),
         secondary_y=False,
     )
     fig.add_trace(
-        go.Scatter(x=data[data['municipio']==value]["anio_corte"], y=data[data['municipio']==value]["inversion_conectividad"], name="Inversión MinTC - Conectividad"),
+        go.Scatter(x=data[data['municipio_fixed_name']==value]["anio_corte"], y=data[data['municipio_fixed_name']==value]["inversion_conectividad"], name="Inversión MinTC - Conectividad"),
         secondary_y=False,
     )
     fig.add_trace(
-        go.Scatter(x=data[data['municipio']==value]["anio_corte"], y=data[data['municipio']==value]["inversion"], name="Inversión MinTC - Total"),
+        go.Scatter(x=data[data['municipio_fixed_name']==value]["anio_corte"], y=data[data['municipio_fixed_name']==value]["inversion"], name="Inversión MinTC - Total"),
         secondary_y=False,
     )
 
     fig.add_trace(
-        go.Scatter(x=data[data['municipio']==value]["anio_corte"], y=data[data['municipio']==value]["componente_de_resultados"], name="Índice Calidad de Vida"),
+        go.Scatter(x=data[data['municipio_fixed_name']==value]["anio_corte"], y=data[data['municipio_fixed_name']==value]["componente_de_resultados"], name="Índice Calidad de Vida"),
         secondary_y=True,
     )
     return fig
@@ -147,8 +147,8 @@ def update_geo_graph(year, type_of_investment):
     Output("stripplot", "figure"),
     Input("x_options_menu_stripplot", "value"),
     Input("y_options_menu_stripplot", "value"))
-def update_stripplot(x_option_name="anio_corte", y_option_name="cobertura_media_neta"):
-    fig = px.strip(df, x=x_option_name, y=y_option_name)
+def update_stripplot(x_option_name="anio_corte", y_option_name="cobertura_neta_en_educacion_media"):
+    fig = px.strip(data, x=x_option_name, y=y_option_name)
     fig.update_xaxes(title=x_option_name)
     fig.update_yaxes(title=y_option_name)
     return fig
